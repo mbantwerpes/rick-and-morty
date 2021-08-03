4a14fb1a7e20e57faaa5b0cd2f19992f45292966
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-03 14:17:10 +0200

    Add utils function to create node elements

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import './style.css';
+import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app !== null) {
+  const body = document.createElement('body');
+  app.append(body);
+
   const main = document.createElement('main');
-  app.append(main);
+  main.className = 'container';
+  body.append(main);
 
   const title = document.createElement('h1');
   title.innerText = 'Rick and Morty';
@@ -20,4 +25,20 @@ if (app !== null) {
   input.placeholder = 'Search...';
 
   main.append(input);
+
+  const card = document.createElement('div');
+  card.className = 'card';
+  const cardTitle = document.createElement('h3');
+  cardTitle.innerText = 'Rick';
+  const cardDescription = document.createElement('p');
+  cardDescription.innerText = 'Lorem ipsum dolor sit amet.';
+  const cardImg = document.createElement('img');
+  cardImg.src =
+    'https://images-na.ssl-images-amazon.com/images/I/91MteSqsrJL.jpg';
+
+  card.append(cardTitle);
+  card.append(cardDescription);
+  card.append(cardImg);
+
+  main.append(card);
 }
