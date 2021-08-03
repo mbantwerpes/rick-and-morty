cda652fc099eaafa172fc32296215a66dd9d61c7
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-03 14:30:16 +0200

    Create htlm nodes with createElement method from utils

--- src/main.ts
+++ src/main.ts
@@ -3,42 +3,51 @@ import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
+const mainElement = createElement('main', {
+  childElements: [
+    createElement('body', {
+      childElements: [
+        createElement('main', {
+          className: 'container',
+          childElements: [
+            createElement('h1', { innerText: 'Rick and Morty' }),
+            createElement('h2', { innerText: 'Lorem ipsum dolor sit amet' }),
+            createElement('input', { placeholder: 'Search...' }),
+          ],
+        }),
+      ],
+    }),
+  ],
+});
+
 if (app !== null) {
-  const body = document.createElement('body');
-  app.append(body);
+  app.append(mainElement);
 
-  const main = document.createElement('main');
-  main.className = 'container';
-  body.append(main);
-
-  const title = document.createElement('h1');
-  title.innerText = 'Rick and Morty';
-
-  main.append(title);
-
-  const subTitle = document.createElement('h2');
-  subTitle.innerText = 'Lorem ipsum dolor sit amet';
-
-  main.append(subTitle);
-
-  const input = document.createElement('input');
-  input.placeholder = 'Search...';
-
-  main.append(input);
-
-  const card = document.createElement('div');
-  card.className = 'card';
-  const cardTitle = document.createElement('h3');
-  cardTitle.innerText = 'Rick';
-  const cardDescription = document.createElement('p');
-  cardDescription.innerText = 'Lorem ipsum dolor sit amet.';
-  const cardImg = document.createElement('img');
-  cardImg.src =
-    'https://images-na.ssl-images-amazon.com/images/I/91MteSqsrJL.jpg';
-
-  card.append(cardTitle);
-  card.append(cardDescription);
-  card.append(cardImg);
-
-  main.append(card);
+  // const body = document.createElement('body');
+  // app.append(body);
+  // const main = document.createElement('main');
+  // main.className = 'container';
+  // body.append(main);
+  // const title = document.createElement('h1');
+  // title.innerText = 'Rick and Morty';
+  // main.append(title);
+  // const subTitle = document.createElement('h2');
+  // subTitle.innerText = 'Lorem ipsum dolor sit amet';
+  // main.append(subTitle);
+  // const input = document.createElement('input');
+  // input.placeholder = 'Search...';
+  // main.append(input);
+  // const card = document.createElement('div');
+  // card.className = 'card';
+  // const cardTitle = document.createElement('h3');
+  // cardTitle.innerText = 'Rick';
+  // const cardDescription = document.createElement('p');
+  // cardDescription.innerText = 'Lorem ipsum dolor sit amet.';
+  // const cardImg = document.createElement('img');
+  // cardImg.src =
+  //   'https://images-na.ssl-images-amazon.com/images/I/91MteSqsrJL.jpg';
+  // card.append(cardTitle);
+  // card.append(cardDescription);
+  // card.append(cardImg);
+  // main.append(card);
 }
