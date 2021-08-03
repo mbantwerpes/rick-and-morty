89e45b816c4b9ba16576b31ed6801764e2d7372b
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-03 14:54:25 +0200

    Setup structure for cdd

--- src/main.ts
+++ src/main.ts
@@ -22,32 +22,4 @@ const mainElement = createElement('main', {
 
 if (app !== null) {
   app.append(mainElement);
-
-  // const body = document.createElement('body');
-  // app.append(body);
-  // const main = document.createElement('main');
-  // main.className = 'container';
-  // body.append(main);
-  // const title = document.createElement('h1');
-  // title.innerText = 'Rick and Morty';
-  // main.append(title);
-  // const subTitle = document.createElement('h2');
-  // subTitle.innerText = 'Lorem ipsum dolor sit amet';
-  // main.append(subTitle);
-  // const input = document.createElement('input');
-  // input.placeholder = 'Search...';
-  // main.append(input);
-  // const card = document.createElement('div');
-  // card.className = 'card';
-  // const cardTitle = document.createElement('h3');
-  // cardTitle.innerText = 'Rick';
-  // const cardDescription = document.createElement('p');
-  // cardDescription.innerText = 'Lorem ipsum dolor sit amet.';
-  // const cardImg = document.createElement('img');
-  // cardImg.src =
-  //   'https://images-na.ssl-images-amazon.com/images/I/91MteSqsrJL.jpg';
-  // card.append(cardTitle);
-  // card.append(cardDescription);
-  // card.append(cardImg);
-  // main.append(card);
 }
