28dae484efd60c70c1c6c9ae5536f34f99fff509
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-03 12:18:45 +0200

    Add title, subtitle and search input

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,21 @@ import './style.css';
 const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app !== null) {
-  app.innerHTML = `
-    <h1>Hello Vite!</h1>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  `;
+  const main = document.createElement('main');
+  app.append(main);
+
+  const title = document.createElement('h1');
+  title.innerText = 'Rick and Morty';
+
+  main.append(title);
+
+  const subTitle = document.createElement('h2');
+  subTitle.innerText = 'Lorem ipsum dolor sit amet';
+
+  main.append(subTitle);
+
+  const input = document.createElement('input');
+  input.placeholder = 'Search...';
+
+  main.append(input);
 }
